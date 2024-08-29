cf1c7f91b18445aebfbc18044448f368e69be05d
Wassim <512.amosimo@gmail.com>
2024-08-29 17:29:37 +0100

    fix auth token

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -2,7 +2,16 @@ import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar/SideBar";
 import "./AdminLayout.css";
 import AdminCheck from "../components/AdminCheck/AdminCheck";
+import axios from "axios";
 const AdminLayout = () => {
+  const token = localStorage.getItem("token");
+
+  console.log(token)
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
+
+
   return (
     <>
       <AdminCheck> </AdminCheck>
